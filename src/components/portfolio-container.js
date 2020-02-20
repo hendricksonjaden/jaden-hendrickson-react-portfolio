ba941865654a2bc5e89268a411635ae63e9779c2
jaden hendrickson <hendricksonjaden@yahoo.com>
2020-02-20 16:29:30 -0700

    Changed app.js and created a portfolio-container

---
+++ src/components/portfolio-container.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'; 
+
+export default class portfolioContainer extends Component {
+  render () {
+    return (
+      <div className='portfolioContainer'>
+        <h2>Portfolio items go here...</h2>
+      </div>
+    );
+  }
+}
