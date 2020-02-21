d4277bb059d8e7870ddb16c2a54757f97b303537
jaden hendrickson <hendricksonjaden@yahoo.com>
2020-02-21 10:48:28 -0700

    Added a new function that will map over data and render a portfolioItem

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -1,17 +1,28 @@
 import React, { Component } from 'react'; 
+
 import PortfolioItem from "./portfolio-item";
 
 export default class portfolioContainer extends Component {
   constructor() {
     super();
-  }
-  render () {
-    console.log("Portfolio container has rendered");
-    return (
-      <div className='portfolioContainer'>
-        <h2>Portfolio items go here...</h2>
 
-        <PortfolioItem />
+    console.log("Portfolio container has rendered");
+  }
+
+  portfolioItems() {
+    const data = ["Upper Case Printing", "Freedom Mailing Services", "Point", "DOMO"];
+
+    return data.map(item => {
+      return <PortfolioItem />
+    })
+  }
+
+  render () {
+    return (
+      <div>
+        <h2>Portfolio items go here updated...</h2>
+
+        {this.portfolioItems()}
       </div>
     );
   }
