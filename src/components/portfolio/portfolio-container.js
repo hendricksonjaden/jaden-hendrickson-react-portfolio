d255a6722cf3fc24fcf7d8c61e5c1abba74f1b35
jaden hendrickson <hendricksonjaden@yahoo.com>
2020-02-21 10:09:39 -0700

    Added a portfolio-items.js

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'; 
+import PortfolioItem from "./portfolio-item";
 
 export default class portfolioContainer extends Component {
   render () {
     return (
       <div className='portfolioContainer'>
         <h2>Portfolio items go here...</h2>
+
+        <PortfolioItem />
       </div>
     );
   }
