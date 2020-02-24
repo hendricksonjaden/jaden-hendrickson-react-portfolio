da04b2177a2e154827178ccdc67fe0eda3f7db8d
jaden hendrickson <hendricksonjaden@yahoo.com>
2020-02-24 15:59:03 -0700

    Added a router for the home and about pages

--- src/components/app.js
+++ src/components/app.js
@@ -1,14 +1,27 @@
 import React, { Component } from "react";
 import moment from "moment";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import PortfolioContainer from './portfolio/portfolio-container';
-import NavigationComponent from './navigation/navigation-container';
+import NavigationContainer from './navigation/navigation-container';
+import Home from './pages/home';
+import About from './pages/about';
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <NavigationComponent />
+        
+        <Router>
+          <div>
+            <NavigationContainer />
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/about-me" component={About} />
+            </Switch>
+          </div>
+        </Router>
+
         <h1>Jaden Hendrickson's Portfolio</h1>
         <div>
           {moment().format('MMMM Do YYYY, h:mm:ss a')}
---
+++ src/components/pages/about.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default function() {
+  return (
+    <div>
+      About
+    </div>
+  );
+}
+
+
