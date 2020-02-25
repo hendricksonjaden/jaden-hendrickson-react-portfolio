b8c880e73b4fd850635fd14d1b2b48ba5c9d776d
jaden hendrickson <hendricksonjaden@yahoo.com>
2020-02-25 15:18:29 -0700

    Organizing Portfolio API Query into the Portfolio Container Class

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react'; 
+import React, { Component } from 'react';
+import axios from 'axios'; 
 
 import PortfolioItem from "./portfolio-item";
 
@@ -17,7 +18,8 @@ export default class PortfolioContainer extends Component {
       ]
     };
   
-      this.handleFilter = this.handleFilter.bind(this);
+    this.handleFilter = this.handleFilter.bind(this);
+    this.getPortfolioItems = this.getPortfolioItems.bind(this);
   }
 
   handleFilter(filter) {
@@ -28,16 +30,30 @@ export default class PortfolioContainer extends Component {
     });
   }
 
+  getPortfolioItems() {
+    axios
+      .get("https://jadenhendrickson.devcamp.space/portfolio/portfolio_items")
+      .then((response) => {
+        console.log("response data", response);
+      })
+      .catch((error) => {
+        console.log(error);
+    });
+  }
+
   portfolioItems() {
     return this.state.data.map(item => {
       return <PortfolioItem title={item.title} url={"google.com"} slug={item.slug} />;
     });
   }
 
-  render () {
+  render() {
     if (this.state.isLoading) {
       return <div>Loading...</div>;
     }
+
+    this.getPortfolioItems()
+
     return (
       <div>
         <h2>{this.state.pageTitle}</h2>
@@ -51,8 +67,6 @@ export default class PortfolioContainer extends Component {
         <button onClick={() => this.handleFilter('Enterprise')}>
           Enterprise
         </button>
-
-        {this.portfolioItems()}
 
       </div>
     ); 
