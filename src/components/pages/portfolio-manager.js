618e05e85f3b2911fc4664fd9aee2debeecd0de4
jaden hendrickson <hendricksonjaden@yahoo.com>
2020-02-28 16:32:29 -0700

    Populated the portfolio manager state with API call data

--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -1,12 +1,44 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class PortfolioManager extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      portfolioItems: []
+    }
+  }
+
+  getPortfolioItems() {
+    axios.get("https://jaden.devcamp.space/portfolio/portfolio_items/", { 
+      withCredentials: true 
+    })
+    .then(response => {
+      this.setState({
+        portfolioItems: [...response.data.portfolio_items]
+      });
+    })
+    .catch(error => {
+      console.log('error in getPortfolioItems', error);
+    });
+  }
+
+  componentDidMount() {
+    this.getPortfolioItems();
+  }
+
   render() {
     return (
-      <div>
-        <h1>PortfolioManager</h1>
-        <h1>PortfolioManager</h1>
-      </div>
+      <div className="portfolio-manager-wrapper">
+        <div className="left-column">
+          <h1>Portfolio form....</h1>
+        </div> 
+
+        <div className="right-colum">
+            <h1>Portfolio sidebar....</h1>
+        </div> 
+      </div> 
     );
   }
 }
