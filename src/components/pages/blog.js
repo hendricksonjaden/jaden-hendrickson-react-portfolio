c12d409a494980d9a60779b572f795368d029fcb
jaden hendrickson <hendricksonjaden@yahoo.com>
2020-03-05 16:15:57 -0700

    Converted the functional blog component into a class component

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -1,10 +1,36 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
+import axios from 'axios';
 
 export default class Blog extends Component {
   constructor() {
     super();
+
+    this.state = {
+      blogItems: []
+    }
+
+    this.getBlogItems = this.getBlogItems.bind(this);
   }
+
+  getBlogItems() {
+    axios.get("https://jadenhendrickson.devcamp.space/portfolio/portfolio_blogs", 
+    { withCredentials: true 
+  })
+  .then(response => {
+    console.log(response)
+    this.setState({
+      blogItems: response.data.portfolio_blogs
+    })
+  })
+  .catch(error => {
+    console.log("getBlogItems error", error)
+  })
+}
+
+componentWillMount() {
+  this.getBlogItems()
+}
 
 render() {
   return (
