1e9e0e2d5e53fda1883aad0e5d6fd95d3ae49a6e
jaden hendrickson <hendricksonjaden@yahoo.com>
2020-03-06 13:43:45 -0700

    Getting the Count of Total Records on the Server and Updating the Component State

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -8,7 +8,9 @@ class Blog extends Component {
     super();
 
     this.state = {
-      blogItems: []
+      blogItems: [],
+      totalCount: 0,
+      currentPage: 0
     };
 
     this.getBlogItems = this.getBlogItems.bind(this);
@@ -17,18 +19,28 @@ class Blog extends Component {
 
   activateInfiniteScroll() {
     window.onscroll = () => {
-      console.log("onscroll");
+      if (
+        window.innerHeight + document.documentElement.scrollTop 
+        === document.documentElement.offsetHeight
+      ) {
+        console.log("get more posts");
+      }
     };
   }
 
   getBlogItems() {
+    this.setState ({
+      currentPage: this.state.currentPage + 1
+    })
+
     axios
-      .get("https://jadenhendrickson.devcamp.space/portfolio/portfolio_blogs", {
-        withCredentials: true
+      .get("https://jadenhendrickson.devcamp.space/portfolio/portfolio_blogs", 
+      { withCredentials: true
       })
       .then(response => {
         this.setState({
-          blogItems: response.data.portfolio_blogs
+          blogItems: response.data.portfolio_blogs,
+          totalCount: response.data.meta.total_records
         });
       })
       .catch(error => {
