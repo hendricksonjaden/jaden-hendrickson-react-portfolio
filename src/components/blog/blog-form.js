1e8f03b855448ab4b9960d259254b316c0b0affa
jaden hendrickson <hendricksonjaden@yahoo.com>
2020-03-10 15:25:52 -0600

    Programmatically Added Content to Draft JS

--- src/components/blog/blog-form.js
+++ src/components/blog/blog-form.js
@@ -141,6 +141,9 @@ export default class BlogForm extends Component {
         <div className="one-column">
           <RichTextEditor
             handleRichTextEditorChange={this.handleRichTextEditorChange}
+            editMode={this.props.editMode}
+            contentToEdit={this.props.editMode && this.props.blog.content ?
+            this.props.blog.content : null }
           />
         </div>
 
