27e8b6faa898e1d0c363598734d78ebc8fd60c1b
jaden hendrickson <hendricksonjaden@yahoo.com>
2020-03-11 15:30:30 -0600

    Built Contact Page Styles for the React Portfolio

--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -1,9 +1,45 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import contactPagePicture from "../../../static/assets/images/auth/bio/login.jpg";
 
 export default function() {
   return (
-    <div>
-      Contact
+    <div className="content-page-wrapper"> 
+    <div 
+      className="left-column"
+      style={{
+        background: "url(" + contactPagePicture + ") no-repeat",
+        backgroundSize: "cover",
+        backgroundPosition: "center"
+      }}
+    />
+      <div className="right-column">
+        <div className="contact-bullet-points">
+          <div className="bullet-point-group">
+            <div className="icon">
+            <FontAwesomeIcon icon="phone" />
+            </div>
+
+            <div className="text">801-874-7132</div>
+          </div>
+
+          <div className="bullet-point-group">
+            <div className="icon">
+            <FontAwesomeIcon icon="envelope" />
+            </div>
+
+            <div className="text">developmentjaden@gmail.com</div>
+          </div>   
+
+          <div className="bullet-point-group">
+            <div className="icon">
+            <FontAwesomeIcon icon="map-marked-alt" />
+            </div>
+
+            <div className="text">Orem, UT</div>
+          </div>   
+        </div>
+      </div>
     </div>
   );
 }
