4a1436c4cc60d025ac1c377e5818e9e3e8aeddb5
jaden hendrickson <hendricksonjaden@yahoo.com>
2020-03-11 18:09:21 -0600

    Rendered the Portfolio Detail Data on the Screen

--- src/components/portfolio/portfolio-detail.js
+++ src/components/portfolio/portfolio-detail.js
@@ -1,13 +1,17 @@
 import React, { Component } from "react";
-import axios from 'axios';
+import axios from "axios";
 
 export default class PortfolioDetail extends Component {
   constructor(props) {
-    super(props)
+    super(props);
+
+    this.state = {
+      portfolioItem: {}
+    };
   }
 
   componentWillMount() {
-    this.getPortfolioItem()
+    this.getPortfolioItem();
   }
 
   getPortfolioItem() {
@@ -15,21 +19,34 @@ export default class PortfolioDetail extends Component {
       .get(
         `https://jadenhendrickson.devcamp.space/portfolio/portfolio_items/${
           this.props.match.params.slug
-        }`, 
+        }`,
         { withCredentials: true }
-        )
-        .then(response => {
-      console.log("res", response
       )
-    }).catch(error => {
-      console.log("getportfolioitem error", error);
-    })
+      .then(response => {
+        this.setState({
+          portfolioItem: response.data.portfolio_item
+        });
+      })
+      .catch(error => {
+        console.log("getportfolioitem error", error);
+      });
   }
 
   render() {
+    const {
+      banner_image_url,
+      category,
+      description,
+      logo_url,
+      name,
+      thumb_image_url,
+      url
+    } = this.state.portfolioItem;
+
     return (
       <div>
-        <h2>Portfolio Detail for {this.props.match.params.slug}</h2>
+        <h2>{name}</h2>
+        <p>{description}</p>
       </div>
     );
   }
